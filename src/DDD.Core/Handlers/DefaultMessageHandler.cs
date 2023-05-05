187f6a5d4cf5e27f6b932d1fc986c64cc41e464e
DMPatod <danielmaiaevangelista@gmail.com>
2023-05-05 17:43:44 -0300

    DefaultMessageHandler

---
+++ src/DDD.Core/Handlers/DefaultMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using DDD.Core.Handlers.SHS.RD.CGC.Core.DomainEvents;
+using DDD.Core.Messages;
+using MediatR;
+
+namespace DDD.Core.Handlers;
+
+public class DefaultMessageHandler : IMessageHandler
+{
+    private readonly IMediator _mediator;
+
+    public DefaultMessageHandler(IMediator mediator)
+    {
+        this._mediator = mediator;
+    }
+
+    public Task PublishAsync<T>(T domainEvent, CancellationToken cancellationToken)
+        where T : IEvent
+    {
+        return _mediator.Publish(domainEvent, cancellationToken);
+    }
+
+    public Task SendAsync(ICommand domainCommand, CancellationToken cancellationToken)
+    {
+        return _mediator.Send(domainCommand, cancellationToken);
+    }
+
+    public Task<TResponse> SendAsync<TResponse>(ICommand<TResponse> domainCommand,
+        CancellationToken cancellationToken)
+    {
+        return _mediator.Send(domainCommand, cancellationToken);
+    }
+}
--- src/DDD.Core/Handlers/IMessageHandler.cs
+++ src/DDD.Core/Handlers/IMessageHandler.cs
@@ -4,14 +4,14 @@ namespace DDD.Core.Handlers
 {
     namespace SHS.RD.CGC.Core.DomainEvents
     {
-        public interface IDomainMessageHandler
+        public interface IMessageHandler
         {
-            Task PublishAsync<T>(T domainEvent, CancellationToken cancellationToken)
+            Task PublishAsync<T>(T messageEvent, CancellationToken cancellationToken)
                 where T : IEvent;
 
-            Task SendAsync(ICommand domainCommand, CancellationToken cancellationToken);
+            Task SendAsync(ICommand messageCommand, CancellationToken cancellationToken);
 
-            Task<TResponse> SendAsync<TResponse>(ICommand<TResponse> domainCommand, CancellationToken cancellationToken);
+            Task<TResponse> SendAsync<TResponse>(ICommand<TResponse> messageCommand, CancellationToken cancellationToken);
         }
     }
 }

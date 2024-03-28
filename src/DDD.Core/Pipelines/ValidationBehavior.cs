075c48cb97dfd68af56f995a74df6a51e7c26bf2
DMPatod <danielmaiaevangelista@gmail.com>
2024-03-28 15:34:54 -0300

    add fluentValidation

---
+++ src/DDD.Core/Pipelines/ValidationBehavior.cs
@@ -0,0 +1,27 @@
+﻿using DDD.Core.Messages;
+using FluentValidation;
+using MediatR;
+
+namespace DDD.Core.Pipelines
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var ctx = new ValidationContext<TRequest>(request);
+
+            var failures = _validators.Select(x => x.Validate(ctx)).SelectMany(x => x.Errors)
+                .Where(x => x is not null).ToArray();
+
+            return failures.Any() ? throw new ValidationException(failures) : next();
+        }
+    }
+}
